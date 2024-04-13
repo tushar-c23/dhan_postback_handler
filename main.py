e340d63f395199d2ce72a652cec1f86b251a3f14
Tushar Choudhary <tusharc.cse@gmail.com>
2024-04-14 02:16:50 +0530

    MOD: CORS

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
